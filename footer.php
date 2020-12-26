468e4769b6178a3be3e855604db3718ddb22a640
Ilija Dimitrijevic <ika9.dimitrijevic@gmail.com>
2020-12-26 16:56:51 +0100

    dodavanje footera

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer" class="footer">
+  <div class="container text-center">
+
+  <h3><b>Pratite nas na drustvenim mrezama</b></h3>
+
+  <ul class="social-links">
+    <li><a href="https://twitter.com/ChampionsLeague?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><i class="fa fa-twitter fa-fw"></i></a></li>
+    <li><a href="https://www.facebook.com/ChampionsLeague/" target="_blank"><i class="fa fa-facebook fa-fw"></i></a></li>
+    <li><a href="#link"><i class="fa fa-google-plus fa-fw"></i></a></li>
+    <li><a href="https://www.instagram.com/championsleague/" target="_blank"><i class="fa fa-instagram fa-fw"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/uefa-champions-league-783110b6" target="_blank"><i class="fa fa-linkedin fa-fw"></i></a></li>
+  </ul>
+
+  All rights reserved, Liga sampiona, 2020
+  </div>
+</footer>
