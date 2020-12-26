9734bf91fa043408558990aa4d5c50ed179d66f9
Ilija Dimitrijevic <ika9.dimitrijevic@gmail.com>
2020-12-26 16:56:00 +0100

    kreiranje menija

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.php"><img class="navbar-brand-logo" src="img/grb.png" /> Liga sampiona</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="index.php">Pocetna</a></li>
+      <li><a href="onama.php">O ligi sampiona</a></li>
+      
+      <li><a href="utakmice.php">Utakmice</a></li>
+      <li><a href="igraci.php">Igraci</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
